5ec2bd96d621482c59f979148e160fbad81e06dc
Vetrivelan <vetri0907.vv@gmail.com>
2021-08-10 17:35:11 +0530

    commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// file system module to perform file operations
+const fs = require('fs')
+ 
+// json data
+const value = require('./data')
 
+// creating data
+const userData = value.data("Vetri",2020,"BTech")
+const userName = "vetri09"
+const userPassword = "vetrivelan"
+// using crypto module to encrypt password
+const crypto = require('crypto')
+const password = crypto.createHmac('sha256', 'secret').update(userPassword).digest('hex')
+
+// // create content in message.txt
+fs.writeFileSync('message.txt', 
+    `Progard Details\n
+    Name -> ${userData.name}
+    Year -> ${userData.Year}
+    Qualification -> ${userData.Qualification}
+    UserName -> ${userName}
+    Password -> ${password}`
+)
